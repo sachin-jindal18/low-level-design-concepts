9ec3b9ecedd415a86b86f548f706d8c212913bef
sachin_jindal <sachin.jindal@paytm.com>
2021-04-07 19:39:32 +0530

    SRP

--- src/oops/SOLID/singleResponsibilityPrinciple/before/EmployeeRepository.java
+++ src/oops/SOLID/singleResponsibilityPrinciple/before/EmployeeRepository.java
@@ -1,11 +1,15 @@
 package oops.SOLID.singleResponsibilityPrinciple.before;
 
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
 import java.util.Arrays;
 import java.util.List;
 
 public class EmployeeRepository {
 
-    public List<Employee> findAll(){
+    public List<Employee> findAll() {
 
         // Employees are kept in memory for simplicity
         Employee anna = new FullTimeEmployee("Anna Smith", 2000);
@@ -15,5 +19,23 @@ public class EmployeeRepository {
         Employee magda = new PartTimeEmployee("Magda Iovan", 920);
 
         return Arrays.asList(anna, billy, steve, magda);
+    }
+
+    public void saveAll(List<Employee> employees) {
+        for (Employee employee : employees) {
+            save(employee);
+        }
+    }
+
+    public void save(Employee employee) {
+        try {
+            Path path = Paths.get(employee.getFullName()
+                    .replace(" ", "_") + ".rec");
+            Files.write(path, employee.serialise().getBytes());
+
+            System.out.println("Saved employee " + employee.toString());
+        } catch (IOException e) {
+            System.out.println("ERROR: Could not save employee. " + e);
+        }
     }
 }
